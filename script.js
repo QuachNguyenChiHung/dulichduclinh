fe381da5d6eb75620b9664dcb7347dbfbf546fa8
QuachNguyenChiHung <bacon3632@gmail.com>
2025-04-10 11:28:20 +0700

    update

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+ 
+function onclickBar(){
+    let bar=document.getElementsByClassName("sidebar")[0]
+    if(bar.style.display=='block'){
+        bar.style.display='none'
+    }else bar.style.display='block'
+}
+
+
